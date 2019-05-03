ee23fe4d0bc652d187f51706c1034bfc65fafd9c
Alex Pappas <apa@anixe.io>
2019-05-03 12:27:38 +0300

    add Monoid to implicitly operate on sum depending on A Type

---
+++ src/main/scala/com/kitro/Monoids.scala
@@ -0,0 +1,27 @@
+package com.kitro
+
+trait Monoid[A] {
+  def op(a1: A, a2: A): A
+  def zero: A
+}
+
+object Monoid {
+
+  implicit val intMonoid = new Monoid[Int] {
+    def op(a1: Int, a2: Int) = a1 + a2
+//    val sum = f = (x: Int, y: Int) => x + y
+    val zero = 0
+  }
+
+  implicit val stringMonoid = new Monoid[String] {
+    def op(a1: String, a2: String) = a1 + a2
+
+    val zero = ""
+  }
+
+  implicit def listMonoid[A]: Monoid[List[A]] = new Monoid[List[A]] {
+    def op(a1: List[A], a2: List[A]) = a1 ++ a2
+
+    val zero = Nil
+  }
+}
